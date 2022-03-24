14f30d59eeb07d8fed431a102c0e1fc42ee0bc01
Vivek Singh Adhikari <vivek20adhikari@gmail.com>
2022-03-24 20:58:49 +0530

    new

---
+++ s.java
@@ -0,0 +1,13 @@
+public class s
+{
+    void area1(double r)
+	{
+		double ar=3.14*r*r;
+		System.out.println("Area Of Circle : "+ar);
+	}
+	void area1(double l,double b)
+	{
+		double x=l*b;
+		System.out.println("Area Of Rectangle : "+x);
+	}
+}
